c0e94c2ae95f7750ddc6ecef8c96117cfa1a7049
alicitra <alicitra@umd.edu>
2026-02-19 10:14:39 -0500

    perlin clouds

--- IMDM Assignment 3/Assets/Scripts/Cloud.cs
+++ IMDM Assignment 3/Assets/Scripts/Cloud.cs
@@ -1,22 +1,40 @@
 
 using UnityEngine;
 
-public class Cloud : MonoBehaviour
-{
-    public float opacity;
-    public float speed;
-    public Vector3 direction;
+public class Cloud : MonoBehaviour {
+    private Vector3 origin;        
+    private float x_offset, y_offset, z_offset;
 
+    /* Feel free to change these */
+    public float wanderRadius = 3f;
+    public float noiseSpeed = 0.2f;
+    ///////////////////////////////
 
+    public void Initialize(Vector3 startPosition){
+        origin = startPosition;
+        transform.position = startPosition;
 
-    public void Initialize(float opacity, float speed, Vector3 direction){
-        this.opacity = opacity;
-        this.speed = speed;
-        this.direction = direction;
+        // How much can this ball move?
+        x_offset = Random.Range(0f, 999f);
+        y_offset = Random.Range(0f, 999f);
+        z_offset = Random.Range(0f, 999f);
     }
 
-    void Update()
-    {
-        transform.position += direction * speed * Time.deltaTime;
+    void Update(){
+        // Sample Perlin noise at slowly-advancing time values, unique per ball
+        float t = Time.time * noiseSpeed;
+
+
+        /* Perlin Noise: 
+            https://adrianb.io/2014/08/09/perlinnoise.html  
+                --> Perfect for clouds
+        */
+
+        // This implementation returns [0,1] but we want [-1,1]
+        float dx = (Mathf.PerlinNoise(x_offset, t) - 0.5f) * 2f;
+        float dy = (Mathf.PerlinNoise(y_offset + 100f, t) - 0.5f) * 2f;
+        float dz = (Mathf.PerlinNoise(z_offset + 200f, t) - 0.5f) * 2f;
+
+        transform.position = origin + new Vector3(dx, dy, dz) * wanderRadius;
     }
 }
--- IMDM Assignment 3/Assets/Scripts/CloudController.cs
+++ IMDM Assignment 3/Assets/Scripts/CloudController.cs
@@ -1,58 +1,40 @@
-
 using UnityEngine;
-using System;
-
 
 public class CloudController : MonoBehaviour {
 
-/* CHANGE THESE FIELDS AS NEEDED */
-public static float[] PHASE_STARTS = {0.0f, 5.0f, 10.0f, 15.0f};
-public static int NUM_CLOUDS = 100;
-///////////////////////////////////
+    /* Feel free to change these */
+    public static float[] PHASE_STARTS = {0.0f, 5.0f, 10.0f, 15.0f};
+    public static int NUM_CLOUDS = 100;
+    ///////////////////////////////
 
-
-
-Cloud[] clouds;
-Vector3[] cloudsInitialPositions;
-int phase = 0;  // Where in the song are we?
-
-
+    Cloud[] clouds;
+    int phase = 0;
 
     void Start(){
+
         clouds = new Cloud[NUM_CLOUDS];
-        cloudsInitialPositions = new Vector3[NUM_CLOUDS];
 
-        // Sequentially fill the Clouds array with new clouds {Opacity, Speed, Direction}
+        // Spawn all of the clouds
         for(int i = 0; i < NUM_CLOUDS; i++){
+            GameObject cloudObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            cloudObj.name = "Cloud";
+            Cloud cloud = cloudObj.AddComponent<Cloud>();
 
-        GameObject cloudObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        cloudObj.name = "Cloud";
 
-        Cloud cloud = cloudObj.AddComponent<Cloud>();
-
-        cloud.Initialize(2.0f, 2.0f, new Vector3(UnityEngine.Random.Range(0f, 360f), UnityEngine.Random.Range(0f, 360f),
-                UnityEngine.Random.Range(0f, 360f)));
-
-        clouds[i] = cloud;
-
-        cloudsInitialPositions[i] = new Vector3(UnityEngine.Random.Range(-20f, 20f), UnityEngine.Random.Range(5f, 20f),
-                UnityEngine.Random.Range(-20f, 20f));
-
-        clouds[i].transform.position = cloudsInitialPositions[i];
+            // Put the cloud somewhere using a random Vector3 param (in Cloud.cs)
+            Vector3 startPos = new Vector3(Random.Range(-20f, 20f), Random.Range(5f, 20f), Random.Range(-20f, 20f));
+            cloud.Initialize(startPos);
+ 
+            clouds[i] = cloud;
+        }
     }
-
-}
-
 
     void Update(){
 
-        // Update the current phase if necessary.
         if(phase <= 3 && Time.realtimeSinceStartupAsDouble >= PHASE_STARTS[phase]){
             phase++;
-            Debug.Log("Entering Phase " + phase + "\n");
+            Debug.Log("Entering Phase " + phase);
         }
 
     }
-
-
 }
